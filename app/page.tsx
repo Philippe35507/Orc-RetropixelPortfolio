0b7cab5b5da23b92c0e59ade05a46b8432f33576
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-06-11 12:16:32 +0000

    Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className="flex">
-        <button type="button" className="nes-btn is-primary">
-          Primary
-        </button>
-        <button type="button" className="nes-btn is-success">
-          Success
-        </button>
-        <button type="button" className="nes-btn is-warning">
-          Warning
-        </button>
+    <main className="flex flex-col items-center justify-between p-24 gap-10">
+      <h1>Are you looking to become a true web dev warrior?</h1>
+      <div className="nes-container with-title is-centered bg-blue-200 w-[45rem] ">
+        <p className="title">Greetings!</p>
+        <p>
+          With 15 years of web development experience under my belt, I have a
+          lot of tech stories to share. Join me as I passionately delve into my
+          coding and task completion methods, offering valuable knowledge and
+          insights along the way.
+        </p>
       </div>
     </main>
   );
