06f9f8493275113b7754beaf6a6d79e4f3f04833
Philippe <55062877+Philippe35507@users.noreply.github.com>
2024-06-11 14:25:08 +0000

    Pixel Orc Subscribe! onMouseEnter

--- app/components/ui/subscribe-orc/subscribe-orc.tsx
+++ app/components/ui/subscribe-orc/subscribe-orc.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function SubscribeOrc() {
+  const [isShowDialog, setIsShowDialog] = useState<boolean>(false);
+
   return (
     <div className="fixed bottom-0 right-0">
-      <div className="nes-balloon from-right w-44">
-        <p className="text-xs">Subscribe!</p>
-      </div>
-      <Image src={"/pixel-orc.webp"} width={300} height={300} alt="Orc" />
+      {isShowDialog && (
+        <div className="nes-balloon from-right w-44">
+          <p className="text-xs">Subscribe!</p>
+        </div>
+      )}
+      <Link href={"#"}>
+        <Image
+          onMouseEnter={() => setIsShowDialog(true)}
+          onMouseLeave={() => setIsShowDialog(false)}
+          src={"/pixel-orc.webp"}
+          width={300}
+          height={300}
+          alt="Orc"
+        />
+      </Link>
     </div>
   );
 }
